848fc6b5231adb1efaa9a405ec63c8010beaadce
KKene <30391514+KKene@users.noreply.github.com>
2021-11-18 19:58:01 -0500

    Added Argument Parser

--- webcrawler.py
+++ webcrawler.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
-import argparse
-import socket
-import ssl
+import argparse, socket, ssl
+
+### Debug ###
+# DEBUG = True
+DEBUG = False
+
+### Argument Parser ####
+parser = argparse.ArgumentParser(description='crawl the web')
+parser.add_argument('username', type=str, help="Username")
+parser.add_argument('networks', type=str, help="Password")
+args = parser.parse_args()
