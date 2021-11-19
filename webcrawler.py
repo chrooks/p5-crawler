a2f6ebad904bda68fa4e8720e020511ac48e138e
KKene <30391514+KKene@users.noreply.github.com>
2021-11-18 20:34:34 -0500

    Attempting to POST

--- webcrawler.py
+++ webcrawler.py
@@ -9,5 +9,39 @@ DEBUG = False
 ### Argument Parser ####
 parser = argparse.ArgumentParser(description='crawl the web')
 parser.add_argument('username', type=str, help="Username")
-parser.add_argument('networks', type=str, help="Password")
+parser.add_argument('password', type=str, help="Password")
 args = parser.parse_args()
+
+######################################################################################
+
+### Setting up Host and Port ###
+host = "https://fakebook.3700.network/accounts/login/?next=/fakebook/"
+port = 443
+
+### Creating the POST Request ###
+headers = """\
+POST /auth HTTP/1.1\r
+Content-Type: {content_type}\r
+Content-Length: {content_length}\r
+Host: {host}\r
+Connection: close\r
+\r\n"""
+
+body = f'userName={args.username}&password={args.password}'
+body_bytes = body.encode('ascii')
+header_bytes = headers.format(
+    content_type="application/x-www-form-urlencoded",
+    content_length=len(body_bytes),
+    host=str(host) + ":" + str(port)
+).encode('iso-8859-1')
+
+payload = header_bytes + body_bytes
+
+### Setting up the socket ###
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  s.connect((socket.gethostbyname(host), port))  # Connects to the socket
+
+context = ssl.create_default_context()
+socket = context.wrap_socket(s, server_hostname=host)
+
+print(socket.sendall(payload))
