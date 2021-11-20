44aa8fc96cc92bbd7072fdfb80f93ac5742d630c
KKene <30391514+KKene@users.noreply.github.com>
2021-11-20 03:13:02 -0500

    Completed login POST request

--- webcrawler.py
+++ webcrawler.py
@@ -5,6 +5,7 @@ import argparse, socket, ssl
 ### Debug ###
 # DEBUG = True
 DEBUG = False
+CRLF = '\r\n'
 
 ### Argument Parser ####
 parser = argparse.ArgumentParser(description='crawl the web')
@@ -12,31 +13,36 @@ parser.add_argument('username', type=str, help="Username")
 parser.add_argument('password', type=str, help="Password")
 args = parser.parse_args()
 
-######################################################################################
-
 ### Setting up Host and Port ###
 host = "fakebook.3700.network"
 port = 443
+cookie = ""
 
-### Creating the POST Request ###
-### FROM: https://stackoverflow.com/questions/28670835/python-socket-client -post-parameters ####
-headers = """\
-POST accounts/login/ HTTP/1.1\r
-Content-Type: {content_type}\r
-Content-Length: {content_length}\r
-Host: {host}\r
-Connection: close\r
-\r\n"""
 
-body = f'next=/fakebook/&username={args.username}&password={args.password}'
-body_bytes = body.encode('ascii')
-header_bytes = headers.format(
-    content_type="application/x-www-form-urlencoded",
-    content_length=len(body_bytes),
-    host=str(host) + ":" + str(port)
-).encode('iso-8859-1')
+# Generates the GET Request with given headers
+def get(domain, referer=""):
+    request = "GET " + domain + " HTTP/1.1" + CRLF + \
+              "Host: " + host + CRLF + \
+              "Referer: " + referer + CRLF + \
+              "Cookie: " + cookie + CRLF + CRLF
+    if DEBUG: print(request)
+    return request
 
-payload = header_bytes + body_bytes
+
+# Generates the POST Request with given headers
+def post(domain, body):
+    request = "POST " + domain + " HTTP/1.1" + CRLF + \
+              "Host: " + host + CRLF + \
+              "Content-Type: application/x-www-form-urlencoded" + CRLF + \
+              "Content-Length: " + str(len(body)) + CRLF + \
+              "Cookie: csrftoken=" + cookie + CRLF + CRLF + \
+              body
+    if DEBUG: print(request)
+    return request
+
+
+################################################################################
+
 
 ### Setting up the socket ###
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -45,8 +51,17 @@ s.connect((socket.gethostbyname(host), port))  # Connects to the socket
 context = ssl.create_default_context()
 socket = context.wrap_socket(s, server_hostname=host)
 
-print(payload)
+### GET Request for login page (to get csrf token) ###
+socket.send(get(domain='/accounts/login/?next=/fakebook/').encode())
+login_page = socket.recv(3000).decode()
 
-socket.sendall(payload)
+### Getting the csrf (cookie) from the login page ###
+csrf_index = login_page.index(
+    "csrfmiddlewaretoken\" value=\"") + 28  # length of string
+cookie = login_page[csrf_index:].split("\"")[0]
 
-print(socket.recv().decode())
+### Creating the POST Request ###
+body = f'next=/fakebook/&username={args.username}&password={args.password}&csrfmiddlewaretoken={cookie}'
+socket.send(post(domain='/accounts/login/', body=body).encode())
+
+print(socket.recv(3000).decode())
