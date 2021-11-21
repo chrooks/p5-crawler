33c3549c137cfea4f361984a4fbab918fd25b001
KKene <30391514+KKene@users.noreply.github.com>
2021-11-21 14:30:50 -0500

    removed gzip for now

--- webcrawler.py
+++ webcrawler.py
@@ -35,10 +35,10 @@ def get(domain, referer="", has_cookie=False):
     else:
         cookie_or_csrf = "csrftoken=" + CSRF
 
+    ''' Implement gzip encoding '''
     request = "GET " + domain + " HTTP/1.1" + CRLF + \
               "Host: " + HOST + CRLF + \
               "Referer: " + referer + CRLF + \
-              "Accept-Encoding: gzip" + CRLF + \
               "Cookie: " + cookie_or_csrf + CRLF + CRLF
     if DEBUG: print(request)
     return request
@@ -63,6 +63,10 @@ def post(domain, body, has_cookie=False):
     #if DEBUG: print(request)
     return request
 
+
+# Get the csrfmiddlewaretoken from given html
+def get_csrfmiddlewaretoken():
+
 ################################################################################
 
 
@@ -77,26 +81,32 @@ socket = context.wrap_socket(s, server_hostname=HOST)
 socket.send(get(domain='/accounts/login/?next=/fakebook/').encode())
 page = socket.recv(3000).decode()
 
-### Parsing the login page to find the csrf ###
-csrf_index = page.index(
-    "csrfmiddlewaretoken\" value=\"") + 28  # length of string
-CSRF = page[csrf_index:].split("\"")[0]
-
-### Creating the POST Request ###
-body = f'next=/fakebook/&username={args.username}&password={args.password}&csrfmiddlewaretoken={CSRF}'
-socket.send(post(domain='/accounts/login/', body=body).encode())
-response = socket.recv(3000).decode()
-
-if DEBUG: print(f"POST REQUEST RESPONSE\n{response}")
-
-### Parsing the POST Response to find the cookie ###
-cookie_index = response.index("Set-Cookie: sessionid=") + 22  # length of string
-COOKIE = response[cookie_index + 1:].split(";")[0]
-
-### GET Request for homepage ###
-''' Make helper to get the current location of the domain from the POST'''
-referer = 'https://fakebook.3700.network/accounts/login/?next=/fakebook/'
-socket.send(get(domain='/fakebook/', referer=referer, has_cookie=True).encode())
-page = socket.recv(3000).decode()
-
 print(page)
+
+# ### Parsing the login page to find the csrf ###
+# ''' Make helper to get the csrf'''
+# csrf_index = page.index("csrfmiddlewaretoken\" value=\"") + 28
+# CSRF = page[csrf_index:].split("\"")[0]
+#
+# ### Creating the POST Request ###
+# body = f'next=/fakebook/&username={args.username}&password={args.password}&csrfmiddlewaretoken={CSRF}'
+# socket.send(post(domain='/accounts/login/', body=body).encode())
+# response = socket.recv(3000).decode()
+#
+# if DEBUG: print(f"POST REQUEST RESPONSE\n{response}")
+#
+# ### Parsing the POST Response to find the cookie ###
+# ''' Make helper to get the cookie'''
+# cookie_index = response.index("Set-Cookie: sessionid=") + 22
+# COOKIE = response[cookie_index + 1:].split(";")[0]
+#
+# ''' Use helper to get the csrf'''
+#
+#
+# ### GET Request for homepage ###
+# ''' Make helper to get the current location of the domain from the POST'''
+# referer = 'https://fakebook.3700.network/accounts/login/?next=/fakebook/'
+# socket.send(get(domain='/fakebook/', referer=referer, has_cookie=True).encode())
+# page = socket.recv(3000).decode()
+#
+# print(page)
